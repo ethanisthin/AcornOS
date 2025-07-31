4fd82842dd1525c54ce02b96ce3ca13f109c1fd8
ethanisthin <ethanalmeida184@gmail.com>
2025-07-31 08:12:49 -0600

    fixed interrupt handling, was causing issues with reading keyboard input

--- idt.asm
+++ idt.asm
@@ -5,26 +5,24 @@ global irq0_handler
 global irq1_handler
 
 extern idt_desc
-extern irq_handler
+extern irq_handler ; C function: void irq_handler(int irq)
 
 idt_load:
     lidt [idt_desc]
     ret
 
-
 irq0_handler:
-    pusha
-    push 0
-    call irq_handler
-    add esp, 4
-    popa
-    iret
+    pusha                    
+    push dword 0                                
+    call irq_handler         
+    add esp, 4               
+    popa                     
+    iret                    
 
 irq1_handler:
-    pusha
-    cld
-    push 1
-    call irq_handler
-    add esp, 4
-    popa
-    iret
+    pusha                    
+    push dword 1             
+    call irq_handler         
+    add esp, 4               
+    popa                     
+    iret                     
