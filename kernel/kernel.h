4149d286d9bda93e094b61f872cfa1cbcde05cf7
ethanisthin <ethanalmeida184@gmail.com>
2025-08-08 02:45:27 -0600

    added type definitions for standard integer types

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -2,7 +2,12 @@
 #define KERNEL_H
 
 #include <stddef.h>
-
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
 
 void _start();
 
