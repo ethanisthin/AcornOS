54155f499164e7c4c8576fe6cef751ef8a0e623c
ethanisthin <ethanalmeida184@gmail.com>
2025-08-10 20:38:40 +0300

    removed history feature since it was breaking the shell, will add later

--- kernel/shell.c
+++ kernel/shell.c
@@ -5,54 +5,16 @@
 
 /* Defintions */
 #define MAX_INPUT 128
-#define CMD_HISTORY 5
 
 /* Global Variables */
 static char input_buffer[MAX_INPUT];
 static int input_pos = 0;
-static char history[CMD_HISTORY][MAX_INPUT];
-static int history_pos = 0;
-static int history_count = 0;
 
 /* Function Declarations */
 static int str_len(const char* str);
 static int str_compare(const char* a, const char* b);
-static int str_ncmp(const char* a, const char* b, int n);
-static void str_copy(char* dest, const char* src);
 static void print_int(int num);
-static void add_history(const char* cmd);
-static void show_history();
-static void help_cmd();
-static void clear_cmd();
-static void echo_cmd(char* text);
-static void ls_cmd();
-static void fs_test_cmd();
-static void cat_cmd(char* filename);
-static void touch_cmd(char* filename);
 
-static int str_len(const char* str) {
-    int len = 0;
-    while (str[len]) len++;
-    return len;
-}
-
-static int str_compare(const char* a, const char* b) {
-    while (*a && *a == *b) { a++; b++; }
-    return *a - *b;
-}
-
-static int str_ncmp(const char* a, const char* b, int n) {
-    for (int i = 0; i < n; i++) {
-        if (a[i] != b[i]) return a[i] - b[i];
-        if (a[i] == '\0') return 0;
-    }
-    return 0;
-}
-
-static void str_copy(char* dest, const char* src) {
-    while (*src) *dest++ = *src++;
-    *dest = '\0';
-}
 
 static void print_int(int num) {
     char buffer[16];
@@ -78,84 +40,37 @@ static void print_int(int num) {
     }
 }
 
-static void add_history(const char* cmd) {
-    if (!cmd || cmd[0] == '\0') return;
-    
-    str_copy(history[history_pos % CMD_HISTORY], cmd);
-    history_pos++;
-    if (history_count < CMD_HISTORY) history_count++;
+static int str_len(const char* str) {
+    int len = 0;
+    while (str[len]) len++;
+    return len;
 }
 
-static void show_history() {
-    println("\nCommand history:");
-    int start = history_pos > CMD_HISTORY ? history_pos % CMD_HISTORY : 0;
-    
-    for (int i = 0; i < history_count; i++) {
-        int idx = (start + i) % CMD_HISTORY;
-        print(" ");
-        print_int(i + 1);
-        print(". ");
-        println(history[idx]);
-    }
+static int str_compare(const char* a, const char* b) {
+    while (*a && *a == *b) { a++; b++; }
+    return *a - *b;
 }
 
-static void help_cmd() {
+void shell_print_prompt() {
+    print("@AcornOS$~: ");
+    mark_inp_start(); 
+}
+
+void help_cmd() {
     println("\nAvailable commands:");
-    println("help     - Show this help");
+    println("help     - Display available commands");
     println("clear    - Clear screen");
-    println("echo     - Print text");
-    println("history  - Show command history");
-    println("ls       - List files");
-    println("cat      - Display file contents");
-    println("touch    - Create empty file");
-    println("fstest   - Test file system");
+    println("fstest   - Test for file system");
+    println("ls       - List files in system");
 }
 
-static void clear_cmd() {
+void clear_cmd() {
     clr_scr();
     shell_print_prompt();
 }
 
-static void echo_cmd(char* text) {
-    print("\n");
-    print(text);
-}
-
-static void ls_cmd() {
-    struct fat12_dir_entry entries[32];
-    int count = fat12_list_directory(entries, 32);
-    
-    if (count < 0) {
-        println("\nError reading directory");
-        return;
-    }
-    
-    if (count == 0) {
-        println("\nDirectory is empty");
-        return;
-    }
-    
-    println("\nFiles:");
-    for (int i = 0; i < count; i++) {
-        for (int j = 0; j < 8 && entries[i].filename[j] != ' '; j++) {
-            enter_char(entries[i].filename[j]);
-        }
-        
-        if (entries[i].extension[0] != ' ') {
-            enter_char('.');
-            for (int j = 0; j < 3 && entries[i].extension[j] != ' '; j++) {
-                enter_char(entries[i].extension[j]);
-            }
-        }
-        
-        print(" (");
-        print_int(entries[i].file_size);
-        println(" bytes)");
-    }
-}
-
-static void fs_test_cmd() {
-    println("\n FAT12 test");
+void fs_test_cmd() {
+    println("\n FS test");
     
     if (fat12_is_initialized()) {
         println("File system initialized");
@@ -195,54 +110,80 @@ static void fs_test_cmd() {
         println("Failed");
     }
     
+    print("Testing file write... ");
+    const char* test_content = "THIS WORKS BAHHHHHHHHHHH";
+    int bytes_written = fat12_write_file("write_test.txt", (void*)test_content, str_len(test_content));
+    if (bytes_written > 0) {
+        println("Success");
+        print("Wrote ");
+        print_int(bytes_written);
+        println(" bytes");
+    } else {
+        println("Failed");
+    }
+    
+    print("Testing written file read... ");
+    char read_buffer[128];
+    int read_bytes = fat12_read_file("write_test.txt", read_buffer, 127);
+    if (read_bytes > 0) {
+        read_buffer[read_bytes] = '\0';
+        println("Success");
+        print("Content: ");
+        println(read_buffer);
+    } else {
+        println("Failed");
+    }
+    
     println("Test Complete");
 }
 
-static void cat_cmd(char* filename) {
-    if (!filename || filename[0] == '\0') {
-        println("\nUsage: cat <filename>");
+void ls_cmd() {
+    struct fat12_dir_entry entries[32];
+    int count = fat12_list_directory(entries, 32);
+    
+    if (count < 0) {
+        println("\nError reading directory");
         return;
     }
     
-    char buffer[512];
-    int bytes_read = fat12_read_file(filename, buffer, 511);
-    
-    if (bytes_read < 0) {
-        print("\nFile not found: ");
-        println(filename);
+    if (count == 0) {
+        println("\nDirectory is empty");
         return;
     }
     
-    if (bytes_read == 0) {
-        println("\nFile is empty");
-        return;
-    }
-    
-    buffer[bytes_read] = '\0';
-    println("");
-    print(buffer);
-}
-
-static void touch_cmd(char* filename) {
-    if (!filename || filename[0] == '\0') {
-        println("\nUsage: touch <filename>");
-        return;
-    }
-    
-    if (fat12_create_file(filename, 0x20) == 0) {
-        print("\nCreated file: ");
-        println(filename);
-    } else {
-        print("\nFailed to create file: ");
-        println(filename);
+    println("\nFiles:");
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < 8 && entries[i].filename[j] != ' '; j++) {
+            enter_char(entries[i].filename[j]);
+        }
+        
+        if (entries[i].extension[0] != ' ') {
+            enter_char('.');
+            for (int j = 0; j < 3 && entries[i].extension[j] != ' '; j++) {
+                enter_char(entries[i].extension[j]);
+            }
+        }
+        
+        print(" (");
+        print_int(entries[i].file_size);
+        println(" bytes)");
     }
 }
 
 void execute_command(char* input) {
-    if (str_len(input) == 0) return;
+    if (input == NULL || input[0] == '\0') {
+        shell_print_prompt();
+        return;
+    }
     
-    add_history(input);
-    
+    while (*input == ' ') {
+        input++;
+    }
+
+    if (*input == '\0') {
+        shell_print_prompt();
+        return;
+    }
     if (str_compare(input, "help") == 0) {
         help_cmd();
     } 
@@ -250,36 +191,21 @@ void execute_command(char* input) {
         clear_cmd();
         return;
     }
-    else if (str_ncmp(input, "echo ", 5) == 0) {
-        echo_cmd(input + 5);
-    }
-    else if (str_compare(input, "history") == 0) {
-        show_history();
+    else if (str_compare(input, "fstest") == 0) {
+        fs_test_cmd();
     }
     else if (str_compare(input, "ls") == 0) {
         ls_cmd();
     }
-    else if (str_compare(input, "fstest") == 0) {
-        fs_test_cmd();
-    }
-    else if (str_ncmp(input, "cat ", 4) == 0) {
-        cat_cmd(input + 4);
-    }
-    else if (str_ncmp(input, "touch ", 6) == 0) {
-        touch_cmd(input + 6);
-    }
     else {
-        println("");
-        print("Unknown command: ");
-        print(input);
-        println("");
-        return;
+        println("\nUnknown command");
     }
     shell_print_prompt();
 }
 
 void shell_process_char(char c) {
-    if (c == '\n') {
+    enter_char(c);
+    if (c == '\n' || c == '\r') {
         input_buffer[input_pos] = '\0';
         execute_command(input_buffer);
         input_pos = 0;
@@ -287,28 +213,16 @@ void shell_process_char(char c) {
     else if (c == '\b') {
         if (input_pos > 0) {
             input_pos--;
-            enter_char(c);
-            update_cursor();
         }
     }
     else if (input_pos < MAX_INPUT - 1) {
         input_buffer[input_pos++] = c;
-        enter_char(c);
-        update_cursor();
     }
-}
-
-void shell_print_prompt() {
-    print("\n@AcornOS:~$ ");
-    mark_inp_start();
-    update_cursor();
 }
 
 void shell_init() {
     input_pos = 0;
-    history_pos = 0;
-    history_count = 0;
     clr_scr();
-    print("AcornOS v0.1 - Type 'help' for commands\n");
+    println("AcornOS v0.1 - Type 'help' for commands");
     shell_print_prompt();
 }
