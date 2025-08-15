224e719d9c667d3ad98ecf9e6598226dc2ca2a23
ethanisthin <ethanalmeida184@gmail.com>
2025-08-15 20:08:59 +0300

    made kernel a little more fancy

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,13 +1,21 @@
-__attribute__((section(".multiboot")))
-const unsigned multiboot_header[] = {
-    0x1BADB002, 
-    0x00000003,  
-    -(0x1BADB002 + 0x00000003) 
-};
+#define VGA_COLOUR_BLACK 0x00
+#define VGA_COLOUR_BLUE 0x01
+#define VGA_COLOUR_GREEN 0x02
+#define VGA_COLOUR_RED 0x04
+#define VGA_COLOUR_WHITE 0x0F
 
-void _start() {
-    volatile char *vga = (volatile char*)0xB8000;
-    vga[0] = 'K'; 
-    vga[1] = 0x1F;
-    while(1);
+void kernel_main() {
+    volatile unsigned short *vga = (volatile unsigned short*)0xB8000;
+
+    for (int i = 0; i < 80 * 25; i++) {
+        vga[i] = (VGA_COLOUR_BLUE << 12) | (VGA_COLOUR_WHITE << 8) | ' ';
+    }
+
+    const char *msg = "AcornOS Kernel lives again!!!!";
+
+    for (int i = 0; msg[i]; i++) {
+        vga[i] = (VGA_COLOUR_GREEN << 8) | msg[i];
+    }
+
+    while (1);
 }
