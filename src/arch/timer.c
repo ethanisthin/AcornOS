4036f5ec2f1badde48e2ef1db84c4a207d7bf754
ethanisthin <ethanalmeida184@gmail.com>
2025-08-16 21:40:33 +0300

    refactor timer module: remove redundant comments and improve code clarity

--- src/arch/timer.c
+++ src/arch/timer.c
@@ -2,7 +2,6 @@
 #include "../drivers/vga.h"
 #include "pic.h"
 
-// Port I/O functions
 static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
@@ -13,15 +12,11 @@ static inline uint8_t inb(uint16_t port) {
     return ret;
 }
 
-// Timer variables
 static uint32_t timer_ticks = 0;
 static uint32_t timer_frequency = 0;
 
-// Timer interrupt handler
 void timer_handler(void) {
     timer_ticks++;
-    
-    // Print a message every second (when ticks % frequency == 0)
     if (timer_ticks % timer_frequency == 0) {
         uint32_t seconds = timer_ticks / timer_frequency;
         vga_printf_colored(VGA_COLOR_GREEN, VGA_COLOR_BLACK,
@@ -30,26 +25,20 @@ void timer_handler(void) {
     }
 }
 
-// Initialize the timer
 void timer_init(uint32_t frequency) {
     timer_frequency = frequency;
     timer_ticks = 0;
-    
-    // Calculate the divisor for the desired frequency
     uint32_t divisor = PIT_FREQUENCY / frequency;
     
-    // Make sure divisor fits in 16 bits
     if (divisor > 65535) {
         divisor = 65535;
         frequency = PIT_FREQUENCY / divisor;
         timer_frequency = frequency;
     }
     
-    // Send command to PIT
     uint8_t command = PIT_SELECT_CHANNEL0 | PIT_ACCESS_LOHI | PIT_MODE_RATE_GEN | PIT_BINARY_MODE;
     outb(PIT_COMMAND, command);
     
-    // Send divisor (low byte first, then high byte)
     outb(PIT_CHANNEL0, divisor & 0xFF);
     outb(PIT_CHANNEL0, (divisor >> 8) & 0xFF);
     
@@ -58,20 +47,17 @@ void timer_init(uint32_t frequency) {
                       frequency, divisor);
 }
 
-// Get current tick count
 uint32_t timer_get_ticks(void) {
     return timer_ticks;
 }
 
-// Get elapsed seconds
 uint32_t timer_get_seconds(void) {
     return timer_ticks / timer_frequency;
 }
 
-// Simple sleep function (busy wait)
 void timer_sleep(uint32_t ms) {
     uint32_t target_ticks = timer_ticks + (ms * timer_frequency / 1000);
     while (timer_ticks < target_ticks) {
-        __asm__ volatile ("hlt");  // Wait for next interrupt
+        __asm__ volatile ("hlt");  
     }
 }
--- src/arch/timer.h
+++ src/arch/timer.h
@@ -3,26 +3,19 @@
 
 #include "../include/kernel/types.h"
 
-// PIT (Programmable Interval Timer) ports
+
 #define PIT_CHANNEL0    0x40
 #define PIT_CHANNEL1    0x41
 #define PIT_CHANNEL2    0x42
 #define PIT_COMMAND     0x43
-
-// PIT frequency (1.193182 MHz)
 #define PIT_FREQUENCY   1193182
-
-// Timer modes
 #define PIT_MODE_ONESHOT    0x00
 #define PIT_MODE_RATE_GEN   0x04
 #define PIT_MODE_SQUARE     0x06
-
-// Command register bits
 #define PIT_SELECT_CHANNEL0 0x00
 #define PIT_ACCESS_LOHI     0x30
 #define PIT_BINARY_MODE     0x00
 
-// Function declarations
 void timer_init(uint32_t frequency);
 void timer_handler(void);
 uint32_t timer_get_ticks(void);
