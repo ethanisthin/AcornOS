ae1855f6b68627587b84c4a2c21fc3354b9b057a
ethanisthin <ethanalmeida184@gmail.com>
2025-08-16 21:40:40 +0300

    update load_kernel and dap to support loading 25 sectors

--- src/bootloader/stage2.asm
+++ src/bootloader/stage2.asm
@@ -46,7 +46,7 @@ load_kernel:
     xor bx, bx
     
     mov ah, 0x02
-    mov al, 24
+    mov al, 25
     mov ch, 0
     mov cl, 6
     mov dh, 0
@@ -76,7 +76,7 @@ enable_a20:
 dap:
     db 0x10
     db 0
-    dw 24
+    dw 25
     dw 0x0000
     dw 0x1000
     dq 5
