c29ed6d68735abef6edccb69b201d65fe91e8e30
ethanisthin <ethanalmeida184@gmail.com>
2025-08-16 11:56:43 +0300

    changed kernel to vga drivers

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,21 +1,8 @@
-#define VGA_COLOUR_BLACK 0x00
-#define VGA_COLOUR_BLUE 0x01
-#define VGA_COLOUR_GREEN 0x02
-#define VGA_COLOUR_RED 0x04
-#define VGA_COLOUR_WHITE 0x0F
+#include "../drivers/vga.h"
 
 void kernel_main() {
-    volatile unsigned short *vga = (volatile unsigned short*)0xB8000;
-
-    for (int i = 0; i < 80 * 25; i++) {
-        vga[i] = (VGA_COLOUR_BLUE << 12) | (VGA_COLOUR_WHITE << 8) | ' ';
-    }
-
-    const char *msg = "AcornOS Kernel lives again!!!!";
-
-    for (int i = 0; msg[i]; i++) {
-        vga[i] = (VGA_COLOUR_GREEN << 8) | msg[i];
-    }
-
+    vga_init();
+    vga_set_clr(VGA_COLOR_GREEN, VGA_COLOR_BLUE);
+    vga_puts("AcornOS Kernel lives again!!!!");
     while (1);
 }
