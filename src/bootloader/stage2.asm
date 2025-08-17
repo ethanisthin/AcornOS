d6c930f65db813cc435ca7f6bf05bdcc3e601bbb
ethanisthin <ethanalmeida184@gmail.com>
2025-08-17 16:06:43 +0300

    changed sector sizes to match kernel size

--- src/bootloader/stage2.asm
+++ src/bootloader/stage2.asm
@@ -13,8 +13,18 @@ start:
     mov sp, 0x7C00
     sti
     
+    mov si, msg_stage2
+    call puts
+    
     call load_kernel
+    
+    mov si, msg_kernel_loaded
+    call puts
+    
     call enable_a20
+
+    mov si, msg_entering_pmode
+    call puts
 
     cli
     lgdt [gdt_descriptor]
@@ -23,7 +33,28 @@ start:
     mov cr0, eax
     jmp 0x08:protected_mode
 
+puts:
+    push si
+    push ax
+    push bx
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+    jmp .loop
+.done:
+    pop bx
+    pop ax
+    pop si
+    ret
+
 load_kernel:
+    mov si, msg_loading_kernel
+    call puts
+    
     mov ah, 0x41
     mov bx, 0x55AA
     mov dl, [boot_drive]
@@ -34,6 +65,9 @@ load_kernel:
     test cl, 1
     jz .use_chs
     
+    mov si, msg_using_lba
+    call puts
+    
     mov si, dap
     mov ah, 0x42
     mov dl, [boot_drive]
@@ -41,14 +75,17 @@ load_kernel:
     jnc .verify_load
     
 .use_chs:
+    mov si, msg_using_chs
+    call puts
+    
     mov ax, 0x1000
     mov es, ax
     xor bx, bx
     
     mov ah, 0x02
-    mov al, 29
+    mov al, 60 ;change here if needed
     mov ch, 0
-    mov cl, 6
+    mov cl, 20
     mov dh, 0
     mov dl, [boot_drive]
     
@@ -64,6 +101,8 @@ load_kernel:
     ret
     
 .disk_error:
+    mov si, msg_disk_error
+    call puts
     cli
     hlt
 
@@ -76,10 +115,10 @@ enable_a20:
 dap:
     db 0x10
     db 0
-    dw 29
+    dw 60 ;change here if needed
     dw 0x0000
     dw 0x1000
-    dq 5
+    dq 20
 
 gdt_start:
     dd 0x00000000, 0x00000000    
@@ -104,7 +143,7 @@ protected_mode:
     cld
     mov esi, 0x10000
     mov edi, 0x100000
-    mov ecx, 5120
+    mov ecx, 8000 ;change here if needed
     rep movsd
     
     call 0x100000
@@ -112,5 +151,13 @@ protected_mode:
 
 bits 16
 boot_drive: db 0
+
+msg_stage2: db 'Stage2 started', 0x0D, 0x0A, 0
+msg_loading_kernel: db 'Loading kernel...', 0x0D, 0x0A, 0
+msg_using_lba: db 'Using LBA', 0x0D, 0x0A, 0
+msg_using_chs: db 'Using CHS', 0x0D, 0x0A, 0
+msg_kernel_loaded: db 'Kernel loaded', 0x0D, 0x0A, 0
+msg_entering_pmode: db 'Entering protected mode', 0x0D, 0x0A, 0
+msg_disk_error: db 'Disk error in stage2!', 0x0D, 0x0A, 0
 
 times 2048-($-$$) db 0
