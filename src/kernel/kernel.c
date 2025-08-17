f75ce7e7147fb3eca55a085f52bced6b5c4af2a0
ethanisthin <ethanalmeida184@gmail.com>
2025-08-17 16:07:57 +0300

    cleaned main function

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,11 +3,13 @@
 #include "../arch/pic.h"
 #include "../drivers/keyboard.h"
 #include "../shell/shell.h"
+#include "../filesystem/fat16.h"
+#include "../drivers/ata.h"
+
 
 void kernel_main() {
-    vga_init();
-    
-    vga_printf("AcornOS v0.1 - type 'help' for commands\n");
+    vga_init();    
+    vga_printf("AcornOS - Shell System\n");
     vga_printf("======================\n\n");
     
     idt_init();
@@ -16,13 +18,19 @@ void kernel_main() {
     
     vga_printf("Enabling interrupts...\n");
     __asm__ volatile ("sti");
-    
-    
-    pic_enable_irq(1);  
-    
+
+    pic_enable_irq(1);    
     vga_printf_colored(VGA_COLOR_GREEN, VGA_COLOR_BLACK,
-                      "System is Ready!\n\n");
-    
+                      "System ready for shell!\n\n");
+
+    ata_init();
+    if (ata_identify()) {
+        vga_printf("ATA drive detected and ready\n");
+    } else {
+        vga_printf_colored(VGA_COLOR_LIGHT_BLUE, VGA_COLOR_BLACK,
+                          "No ATA drive detected (using simulation mode)\n");
+    }
+    fat16_init();
     shell_init();
     shell_run();
 }
