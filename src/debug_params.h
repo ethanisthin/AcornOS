b3fdcfdeb73a4af0bd425600caf25f812a83948f
ethanisthin <ethanalmeida184@gmail.com>
2026-03-01 20:56:54 -0600

    added a new file for debugging purposes, this file allows to turn print statements on/off quickly

---
+++ src/debug_params.h
@@ -0,0 +1,12 @@
+#ifndef DEBUG_PARAMS
+#define DEBUG_PARAMS
+
+/* these are debug flags for the different aspects of the OS */
+#define INT_DEBUG 0 //for interrupts
+#define KB_DEBUG 0 //keyboard driver
+#define FS_DEBUG 0 //file system
+#define PIC_DEBUG 0 //PIC driver
+#define ATA_DEBUG 0 //ATA driver
+#define KN_DEBUG 0 //kernel
+
+#endif
