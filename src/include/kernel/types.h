3a6eb644b1cda807151f891097b39dc663a36dff
ethan-almeida <ethanalmeida184@gmail.com>
2026-05-27 22:46:01 -0600

    switched to using default types

--- src/include/kernel/types.h
+++ src/include/kernel/types.h
@@ -2,17 +2,19 @@
 #define TYPES_H
 
 #include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
-typedef uint32_t size_t;
-typedef int32_t ssize_t;
+// typedef unsigned char uint8_t;
+// typedef unsigned short uint16_t;
+// typedef unsigned int uint32_t;
+// typedef unsigned long long uint64_t;
+// typedef signed char int8_t;
+// typedef signed short int16_t;
+// typedef signed int int32_t;
+// typedef signed long long int64_t;
+// typedef uint32_t size_t;
+// typedef int32_t ssize_t;
 
 typedef enum {
     FALSE = 0,
